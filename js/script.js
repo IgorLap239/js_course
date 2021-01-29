284bfa8c4177547eb99b293b3415c6e0c9f25fc8
Igor <ilaptev@ya.ru>
2021-01-29 11:39:39 +0300

    Commit for second lesson

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-let money = 1;
-let income = 2;
-let addExpenses = 3;
-let deposit = 4;
-let mission = 'Do all homework';
-let period = 'year';
+let money = 150000;
+let income = 'freelance';
+let addExpenses = 'Бензин, коммуналка, продукты';
+let deposit = true;
+let mission = 1000000;
+let period = 12;
 
-alert(mission);
-console.log(period);
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев. Цель заработать ' + mission + ' рублей.');
+console.log(addExpenses.toLowerCase().split(', '));
+let budgetDay = 150000/30;
+console.log(budgetDay)
