fe3efdbb83f133e0a11b261713b628c18a92ad0f
Igor <ilaptev@ya.ru>
2021-03-06 11:58:15 +0300

    Исправления к 26 уроку. Проверка правильности ввода имени и номера телефона. Не отправлятеся пустой Email

--- js/script.js
+++ js/script.js
@@ -361,10 +361,18 @@ window.addEventListener('DOMContentLoaded', () => {
     command();
 
     //валидация форм
+
+
     const validation = () => {
         const form1 = document.getElementById('form1'),
             form2 = document.getElementById('form2'),
-            form3 = document.getElementById('form3');
+            form3 = document.getElementById('form3'),
+            email = document.querySelectorAll('.form-email'),
+            formBtn = document.querySelectorAll('.form-btn');
+
+        email.forEach(item => {
+            item.setAttribute("required", "required");
+        });
 
         const connectFormInput = (e) => {
             const target = e.target;
@@ -373,6 +381,7 @@ window.addEventListener('DOMContentLoaded', () => {
             } else if (target.matches('input[placeholder="Ваше сообщение"]')) {
                 target.value = target.value.replace(/[^А-Яа-я,\.\?!^0-9 ]/g, '');
             } else if (target.matches('input[placeholder="E-mail"]') || target.matches('input[placeholder="Ваш E-mail"]')) {
+                target.setAttribute("required", "required");
                 target.value = target.value.replace(/[^A-Za-z\-@_'`!\.\* ]/g, '');
             } else if (target.matches('input[placeholder="Номер телефона"]') || target.matches('input[placeholder="Ваш номер телефона"]')) {
                 target.value = target.value.replace(/[^0-9\+]/g, '');
@@ -386,9 +395,70 @@ window.addEventListener('DOMContentLoaded', () => {
             target.value = target.value.replace(/^-*/g, '');
             target.value = target.value.replace(/-*$/g, '');
             if (target.matches('input[placeholder="Ваше имя"]')) {
-                target.value = target.value[0].toUpperCase() + target.value.substr(1, ).toLowerCase();
+                if (target.value.length < 2) {
+                    showError(target);
+                    target.closest('form').querySelector('.form-btn').setAttribute("disabled", "disabled");
+                } else {
+                    showSuccess(target);
+                    target.closest('form').querySelector('.form-btn').removeAttribute("disabled");
+                    target.value = target.value[0].toUpperCase() + target.value.substr(1, ).toLowerCase();
+                }
+            }
+            if (target.matches('input[placeholder="Номер телефона"]') || target.matches('input[placeholder="Ваш номер телефона"]')) {
+                if ((target.value[0] !== '+' && target.value.length === 7) || (target.value[0] !== '+' && target.value.length === 11) || (target.value[0] === '+' && target.value.length === 12)) {
+                    showSuccess(target);
+                    target.closest('form').querySelector('.form-btn').removeAttribute("disabled");
+                } else {
+                    showError(target);
+                    target.closest('form').querySelector('.form-btn').setAttribute("disabled", "disabled");
+                }
             }
         };
+
+        const showError = (elem) => {
+            elem.classList.remove('success');
+            elem.classList.add('error');
+            if (elem.nextElementSibling && elem.nextElementSibling.classList.contains('validator-error')) {
+                return;
+            }
+            const errorDiv = document.createElement('div');
+            if (elem.matches('input[placeholder="Ваше имя"]')) {
+                errorDiv.textContent = 'Имя должно состоять минимум из 2 символов';
+            } else if (elem.matches('input[placeholder="Номер телефона"]') || elem.matches('input[placeholder="Ваш номер телефона"]')) {
+                errorDiv.textContent = 'Это не номер телефона';
+            }
+            errorDiv.classList.add('validator-error');
+            elem.insertAdjacentElement('afterend', errorDiv);
+        };
+
+        const showSuccess = (elem) => {
+            elem.classList.remove('error');
+            elem.classList.add('success');
+            if (elem.nextElementSibling && elem.nextElementSibling.classList.contains('validator-error')) {
+                elem.nextElementSibling.remove();
+            }
+        };
+
+        const applyStyle = () => {
+            const style = document.createElement('style');
+            style.textContent = `
+              input.success {
+                border: 2px solid green !important;
+              }
+              input.error {
+                border: 2px solid red  !important;
+              }
+    
+              .validator-error {
+                font-size: 12px;
+                font-family: sans-serif;
+                color: red;
+              }
+              `;
+
+            document.head.appendChild(style);
+        };
+        applyStyle();
 
         const eventListeners = () => {
             form1.addEventListener('input', connectFormInput);
@@ -416,6 +486,7 @@ window.addEventListener('DOMContentLoaded', () => {
             form2 = document.getElementById('form2'),
             form3 = document.getElementById('form3'),
             statusMessage = document.createElement('div');
+
         statusMessage.style.cssText = `font-size: 2rem;
         color: #ffffff;`;
 
